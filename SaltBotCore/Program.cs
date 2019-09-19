0f1d9b2913893953d3846358ed821d6bbc3a9a0a
OfficerSalzig <officersalzig@gmail.com>
2019-09-19 21:28:46 +0200

    Update Program.cs
    Changed "Hello World!" to "Hello Discord!"

--- SaltBotCore/Program.cs
+++ SaltBotCore/Program.cs
@@ -2,11 +2,11 @@
 
 namespace SaltBotCore
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Discord!");
         }
     }
 }
